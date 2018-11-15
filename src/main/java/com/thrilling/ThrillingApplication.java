9b7ac496423ba3950075103d8864216813774443
Halkrine <halkrine@gmail.com>
2018-11-15 20:55:52 +0900

    test

---
+++ src/main/java/com/thrilling/ThrillingApplication.java
@@ -0,0 +1,12 @@
+package com.thrilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThrillingApplication.class, args);
+	}
+}
---
+++ src/main/java/com/thrilling/controller/TestController.java
@@ -0,0 +1,5 @@
+package com.thrilling.controller;
+
+public class TestController {
+
+}
